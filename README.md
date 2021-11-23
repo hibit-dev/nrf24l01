60d814ac63387627ce42f4d9c84f6d1724c2b300
Tigran Mekinyan <tmekinyan@mail.ru>
2021-11-23 22:29:36 +0100

    Add code and documentation

---
+++ README.md
@@ -0,0 +1,23 @@
+# NRF24L01
+Having two or more Arduino boards be able to communicate with each other wirelessly over a distance opens lots of possibilities like remotely monitoring sensor data, controlling robots, home automation and the list goes on. A good, reliable and inexpensive solution is NRF24L01.  
+
+├ src  
+│  ├ receiver  
+│  └ transmitter  
+├ library  
+└ schema  
+
+## Documentation
+You'll find instructions and full documentation on [HiBit](https://www.hibit.dev/posts/22/how-to-use-the-nrf24l01-module-with-arduino). It includes detailed info on how to wire and use the module.
+
+## Credits
+- [nRF24](https://github.com/nRF24)
+
+## Security
+If you discover any security related issues, please email security@hibit.dev instead of using the issue tracker.
+
+## About HiBit
+HiBit is a platform made by and for enthusiasts of the IT world. [On our website](https://www.hibit.dev) you can read and comment on technical articles, tutorials, news ... and everything that may interest you in the computing world.
+
+## License
+The MIT License (MIT). Please see [License File](LICENSE.md) for more information.
---
+++ src/receiver/receiver.ino
@@ -0,0 +1,76 @@
+// -------------------------------------------------
+// Copyright (c) 2021 HiBit <https://www.hibit.dev>
+// -------------------------------------------------
+
+#include "SPI.h"
+#include "RF24.h"
+#include "nRF24L01.h"
+
+#define CE_PIN 9
+#define CSN_PIN 10
+
+#define INTERVAL_MS_SIGNAL_LOST 1000
+#define INTERVAL_MS_SIGNAL_RETRY 250
+
+RF24 radio(CE_PIN, CSN_PIN);
+
+const byte address[6] = "00001";
+
+//NRF24L01 buffer limit is 32 bytes (max struct size)
+struct payload {
+  byte data1;
+  char data2;
+};
+
+payload payload;
+
+unsigned long lastSignalMillis = 0;
+
+void setup()
+{
+  Serial.begin(115200);
+
+  radio.begin();
+  
+  //Append ACK packet from the receiving radio back to the transmitting radio
+  radio.setAutoAck(false); //(true|false)
+  //Set the transmission datarate
+  radio.setDataRate(RF24_250KBPS); //(RF24_250KBPS|RF24_1MBPS|RF24_2MBPS)
+  //Greater level = more consumption = longer distance
+  radio.setPALevel(RF24_PA_MIN); //(RF24_PA_MIN|RF24_PA_LOW|RF24_PA_HIGH|RF24_PA_MAX)
+  //Default value is the maximum 32 bytes
+  radio.setPayloadSize(sizeof(payload));
+  //Act as receiver
+  radio.openReadingPipe(0, address);
+  radio.startListening();
+}
+
+void loop()
+{
+  unsigned long currentMillis = millis();
+
+  if (radio.available() > 0) {
+    radio.read(&payload, sizeof(payload));
+
+    Serial.println("Received");
+
+    Serial.print("Data1:");
+    Serial.println(payload.data1);
+
+    Serial.print("Data2:");
+    Serial.println(payload.data2);
+
+    lastSignalMillis = currentMillis;
+  }
+
+  if (currentMillis - lastSignalMillis > INTERVAL_MS_SIGNAL_LOST) {
+    lostConnection();
+  }
+}
+
+void lostConnection()
+{
+  Serial.println("We have lost connection, preventing unwanted behavior");
+
+  delay(INTERVAL_MS_SIGNAL_RETRY);
+}
